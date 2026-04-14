f7eb28040c81228c9643639a5a1cfc1104aabf45
kalin <kalinkinmaks2000@gmail.com>
2026-04-14 20:05:55 +0300

    Добавил логику игры

--- Arkanoid.Fna/Program.cs
+++ Arkanoid.Fna/Program.cs
@@ -1,11 +1,11 @@
-﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Akanoid.FNA
+namespace Arkanoid.FNA
 {
     static class Program
     {
